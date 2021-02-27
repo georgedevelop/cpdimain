d66d3e6481ff0272d901ad1760e21193fd06193d
giorg <georgea@cpdi-inc.com>
2021-02-27 09:35:46 -0600

    stff

--- cpdi-master/config/routes.rb
+++ cpdi-master/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "dashboard#index"
+  # get "dashboard_unauth#index"
+end
+
+Rails.application.routes.draw do
+  root "dashboard_unauth#index"
 end
